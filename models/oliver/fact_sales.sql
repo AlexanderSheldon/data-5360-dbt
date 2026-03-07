cd2e017a9a60003512ad9037beb4954177ddf8f6
Alex Sheldon <alexsheldon22@gmail.com>
2026-03-07 02:33:43 +0000

    this commit will work trust me bro

--- models/oliver/fact_sales.sql
+++ models/oliver/fact_sales.sql
@@ -6,16 +6,17 @@
 SELECT
 o.order_id as order_key,
 s.store_id as store_key,
-d.date as order_date,
+d.date as date_key,
 c.customer_id as customer_key,
 e.employee_id as employee_key,
+p.product_id as product_key,
 quantity,
-unti_price,
+ol.unit_price,
 total_amount
 FROM {{ source('oliver_landing', 'orders') }} o
     JOIN {{ source('oliver_landing', 'orderline') }} ol ON ol.order_id = o.order_id
-    JOIN {{ ref('oliver_dim_customer')}} c ON c.store_id = o.JOIN o.store_id
-    JOIN {{ ref('oliver_dim_date')}} d ON d.date = o.date
-    JOIN {{ ref('oliver_dim_employee')}} e.employee_id = o.employee_id
-    JOIN {{ ref('oliver_dim_products')}} p ON p.product_id = o.product_id
+    JOIN {{ ref('oliver_dim_customer')}} c ON c.customer_id = o.customer_id
+    JOIN {{ ref('oliver_dim_date')}} d ON d.date = o.order_date
+    JOIN {{ ref('oliver_dim_employee')}} e ON e.employee_id = o.employee_id
+    JOIN {{ ref('oliver_dim_product')}} p ON p.product_id = ol.product_id
     JOIN {{ ref('oliver_dim_store')}} s On s.store_id = o.store_id
