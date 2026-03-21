d0be1e90e5f5314f7c8dd79a44218d6f8058323f
Alex Sheldon <alexsheldon22@gmail.com>
2026-03-21 02:48:22 +0000

    core 6 final for real this time

--- models/oliver/fact_certification.sql
+++ models/oliver/fact_certification.sql
@@ -5,9 +5,9 @@
 
 SELECT {{ dbt_utils.generate_surrogate_key(['e.employee_key', 'd.date_key','c.certification_name']) }} as certification_key, 
     e.employee_key,
-    d.date_key,
+    c.certification_awarded_date as date,
     c.certification_name,
     c.certification_cost
 FROM {{ref('stg_employee_certifications')}} c
-    JOIN {{ref('oliver_dim_date')}} d ON d.date = c.certification_awarded_date 
     JOIN {{ref('oliver_dim_employee')}} e ON c.employee_id = e.employee_id
+    LEFT JOIN {{ref('oliver_dim_date')}} d ON CAST(c.certification_awarded_date AS DATE) = CAST(d.date AS DATE)
